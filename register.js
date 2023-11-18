ed99a18675a2e7a4276f9b7e6cb2ecf49a7f2a4b
eymenwinner <150270961+eymenwinner@users.noreply.github.com>
2023-11-18 19:13:31 +0300

    Update register.js

--- register.js
+++ register.js
@@ -8,49 +8,34 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.querySelector("input[name='email']").value;
         const password = document.querySelector("input[name='password']").value;
         const username = document.querySelector("input[name='username']").value;
-        const bdate = document.querySelector.apply("input[name=")
+        const birthdate = document.querySelector("input[name='birthdate']").value;
 
-        // Doğum tarihi alanını al
-            const birthdate = document.querySelector("input[name='birthdate']").value;
+        // Kullanıcı bilgilerini JSON formatına dönüştürme
+        const userData = {
+            email,
+            password,
+            username,
+            birthdate
+        };
 
-            // Eğer doğum tarihi alanı doluysa ve en az 12 yıl önce bir tarih seçildiyse devam et
-            if (birthdate) {
-                const minBirthdate = new Date();
-                minBirthdate.setFullYear(minBirthdate.getFullYear() - 12);
-
-                const selectedDate = new Date(birthdate);
-
-                if (selectedDate < minBirthdate) {
-                    // Form gönderme işlemleri
-                } else {
-                    alert("Please select a date at least 12 years ago for your birthdate!");
-                }
-            }
-
-        // Kullanıcı bilgilerini XML formatına dönüştürme
-        const userDataXML = `
-            <user>
-                <email>${email}</email>
-                <password>${password}</password>
-                <username>${username}</username>
-                <bdate>${bdate}</bdate>
-            </user>
-        `;
-
-        // data.xml dosyasına XML verisini gönderme
-        fetch('buddies/data.xml', {
+        // Sunucuya POST isteği gönderme
+        fetch('https://example.com/saveUserData', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/xml'
+                'Content-Type': 'application/json'
             },
-            body: userDataXML
+            body: JSON.stringify(userData)
         })
         .then(response => {
             if (response.ok) {
-                window.location.href = "profile.htm";
+                return response.json();
             } else {
-                alert("Error while adding new user data!!!");
+                throw new Error('Error while adding new user data!!!');
             }
+        })
+        .then(data => {
+            // Sunucudan gelen cevaba göre işlem yapılabilir
+            window.location.href = "profile.htm";
         })
         .catch(error => {
             console.error('Hata:', error);
