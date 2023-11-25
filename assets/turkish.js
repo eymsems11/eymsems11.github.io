c546a6df73939ef4910ed23e0944ffb4cadb23b8
eymenwinner <150270961+eymenwinner@users.noreply.github.com>
2023-11-25 21:27:47 +0300

    changes

--- assets/turkish.js
+++
@@ -1,7 +0,0 @@
-var backgroundAudio = document.getElementById('backgroundAudio');
-
-backgroundAudio.addEventListener('error', function() {
-  console.error('Ses dosyası yüklenirken bir hata oluştu.');
-});
-
-backgroundAudio.play();
